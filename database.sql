3e3b7ad60cb25414a921622ad48fc907fcb4b8c1
Ewhitaker2023 <eugenewhitaker2022@gmail.com>
2023-02-03 15:35:14 -0600

    database

---
+++ database.sql
@@ -0,0 +1,42 @@
+alter sequence employee_ID_seq restart with 1;
+
+drop table manager_ticket;
+drop table ticket;
+drop table employee;
+drop table manager;
+drop table managerIDs;
+
+create table employee (
+	ID serial primary key,
+	email varchar(255) not null unique,
+	pass varchar(128) not null
+);
+insert into employee (email, pass) values ('test@test.com', 'test');
+
+create table managerIDs (
+	ID integer primary key
+);
+insert into managerids (ID) values (1),(2),(3);
+
+create table manager ( 
+	ID integer primary key references managerIDs(ID) not null unique,
+	email varchar(255) not null unique,
+	pass varchar(128) not null
+);
+insert into manager (ID, email, pass) values (1, 'admin@test.com', 'test');
+
+create table ticket (
+	ID serial unique,
+	employeeID integer references employee(ID) not null,
+	amount integer not null,
+	description varchar(255) not null,
+	pending boolean not null,
+	status varchar(8),
+	primary key(ID, employeeID)
+);
+insert into ticket (employeeID, amount, description, pending, status) values (1, 100.00, 'test', true, 'pending');
+
+create table manager_ticket (
+	managerID integer references manager(ID) not null,
+	ticketID integer references ticket(ID) not null
+);
